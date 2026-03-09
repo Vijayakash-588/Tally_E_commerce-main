4cfa4d2c22014714091a78674392862bbb04ef04
vijayakash-588 <akashwolf0508@gmail.com>
2026-03-09 11:07:20 +0530

    some changes acquired

--- frontend/src/pages/Invoices.jsx
+++ frontend/src/pages/Invoices.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
-
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
 import { Trash2, Edit2, Play, CheckCircle, Clock, Save, X, Plus, Search, FileDown, Send, CreditCard, ChevronRight, Filter,AlertCircle,Ban,FileText,ArrowLeft,RefreshCcw,Download,Calendar,Eye,List} from 'lucide-react';
@@ -688,9 +687,6 @@ autoTable(doc, {
                                     ))}
                                 </select>
                             </div>
-                            <button className="p-4 bg-slate-50 text-slate-400 hover:text-slate-900 rounded-2xl border border-slate-100 transition-all">
-                                <Download className="w-5 h-5" />
-                            </button>
                         </div>
                     </div>
                 </div>
